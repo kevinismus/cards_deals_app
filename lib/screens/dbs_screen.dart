b83607227f72ce2ab4496a2df1092207aac691dd
kevinismus <kevin.ongko@outlook.sg>
2019-11-10 14:58:26 +0800

    Create DBSScreen widget

---
+++ lib/screens/dbs_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class DBSScreen extends StatefulWidget {
+  final Widget child;
+
+  DBSScreen({Key key, this.child}) : super(key: key);
+
+  @override
+  _DBSScreenState createState() => _DBSScreenState();
+}
+
+class _DBSScreenState extends State<DBSScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: ListView(
+        padding: EdgeInsets.symmetric(vertical: 30.0),
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: 20.0, right: 120.0),
+            child: Text(
+              "Please select a card below:",
+              style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                height: 100.0,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 150.0),
+                  child: Container(
+                    child: Image.asset(
+                      "assets/images/dbs/dbs_logo.png",
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  SizedBox(
+                    height: 100.0,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 50.0, right: 50.0),
+                      child: Container(
+                        child: Image.asset(
+                          "assets/images/dbs/miles/dbs-altitude-card.png",
+                        ),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "DBS Altitude Card:\nThe fastest way to fly",
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cards_deals_app/screens/dbs_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -6,59 +7,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  get left => null;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.symmetric(vertical: 30.0),
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(left: 20.0, right: 120.0),
-              child: Text(
-                "Please select a card below:",
-                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Row(
-              children: <Widget>[
-                SizedBox(
-                  height: 100.0,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 20.0, right: 120.0),
-                    child: Container(
-                      child: Image.asset(
-                        "assets/images/dbs/dbs_logo.png",
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    SizedBox(
-                      height: 100.0,
-                      child: Padding(
-                        padding: EdgeInsets.only(left: 20.0, right: 120.0),
-                        child: Container(
-                          child: Image.asset(
-                            "assets/images/dbs/miles/dbs-altitude-card.png",
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+      body: DBSScreen(),
     );
   }
 }
